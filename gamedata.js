2918553a378adc1617c51256e763e31442f6be78
Oliver Rahner <oliver@rahner.me>
2020-11-21 15:13:43 +0100

    Feature additions

--- gamedata.js
+++ gamedata.js
@@ -1,315 +1,404 @@
-d = 
-	{  
-	   "title": "Nerd Quiz 2017 reloaded",
-	   "intro":"NERD-QUIZ INTRO.mp4",
-	   "categories":  
-		  [
-		  {  
-			 "category": "Klassiker",
-			 "questions": [  
-				{  
-				   "question":"1.1.jpg",
-				   "answers":[  
-						"Loesungen/1.1.1.jpg",
-				"Loesungen/1.1.2.jpg",
-				"Loesungen/1.1.3.jpg",
-				"Loesungen/1.1.4.jpg",
-				"Loesungen/1.1.5.jpg"
-				   ]
+var d =
+	{
+		"title": "Nerd Quiz Winter 2020<br><marquee class='subtitle' scrollamount='10'>&lt;&lt;Xennial Edition&gt;&gt;</marquee>",
+		"intro": "img/intro.mp4",
+		"categories":
+			[
+				{
+					"category": "Classic Gaming",
+					"questions": [
+						{
+							"question": "img/classic-gaming/question1.png",
+							"caption": "Zu welchen Spielekonsolen gehören diese Datenträger?",
+							"answers": [
+								"img/classic-gaming/answer1.png",
+							]
+						},
+						{
+							"question": "img/classic-gaming/question2.mp4",
+							"loop": true,
+							"caption": "Wie lauten die einfachsten Befehle, um auf dem C64 ein Programm von Diskette zu laden und zu starten?",
+							"timer": true,
+							"answers": [
+								"img/classic-gaming/answer2.mp4"
+							]
+						},
+						{
+							"question": "img/logo.png",
+							"caption": "Welches sind die 3 meistverkauften Spielekonsolen aller Zeiten?",
+							"answers": [
+								"img/classic-gaming/answer3.png"
+							]
+						},
+						{
+							"question": "img/classic-gaming/question4.mp4",
+							"caption": "Wann wurde die erste Version dieses Spiels veröffentlicht und wie heißt es?",
+							"answers": [
+								"img/classic-gaming/answer4.jpg"
+							]
+						},
+						{
+							"question": "img/classic-gaming/question5.svg",
+							"caption": "Unter welchem Namen wurde Blizzard 1991 gegründet?",
+							"answers": [
+								"img/classic-gaming/answer5.png"
+							]
+						
+						}
+						
+					]
 				},
-				{  
-				   "question":"1.2.jpg",
-				   "answers":[  
-					  "Loesungen/1.2.jpg"
-				   ]
+				{
+					"category": "Monkey Island",
+					"questions": [
+						{
+							"question": "img/monkey-island/question1.png",
+							"caption": "Vor welchem Monster warnt Guybrush die Kanibalen?",
+							"answers": [
+								"img/monkey-island/answer1.png"
+							]
+						},
+						{
+							"question": "img/logo.png",
+							"caption": "Wie heißen die Teile der Monkey-Island-Saga?",
+							"answers": [
+								"img/monkey-island/answer2.1.png",
+								"img/monkey-island/answer2.2.png",
+								"img/monkey-island/answer2.3.jpg",
+								"img/monkey-island/answer2.4.jpg",
+								"img/monkey-island/answer2.5.jpg"
+							]
+						},
+						{
+							"question": "img/logo.png",
+							"caption": "Wodurch zeichnet sich Stans Sakko aus?",
+							"answers": [
+								{
+									"answer": "img/monkey-island/answer3.mp4"
+								},
+								{
+									"answer": "img/monkey-island/answer3.mp4",
+									"caption": "Die Textur ist an den Bildschirmkoordinaten ausgerichtet, nicht an seinem Modell"
+								}
+							]
+						},
+						{
+							"question": "img/monkey-island/question4.jpg",
+							"caption": "Welcher (deutschen) Stadt sagt man nach, sie habe als Vorlage für dieses Szenenbild gedient?",
+							"answers": [
+								{
+									"answer": "img/monkey-island/answer4.1.webp",
+									"caption": "Rothenburg ob der Tauber"
+								},
+								{
+									"answer": "img/monkey-island/answer4.2.jpg"
+								}
+							
+								
+							]
+						},
+						{
+							"question": "img/monkey-island/question5.mp4",
+							"loop": true,
+							"timer": true,
+							"caption": "Wie lautet die korrekte Antwort auf diese Beleidgung?",
+							"answers": [
+								{
+									"answer": "img/monkey-island/answer5.mp4",
+									"caption": "",
+								}
+							]
+						},						
+
+					]
 				},
-				{  
-				   "question":"1.3.jpg",
-				   "answers":[  
-					  "Loesungen/1.3.jpg"
-				   ]
+				{
+					"category": "Spieletechnik",
+					"questions": [
+						{
+							"question": "img/logo.png",
+							"caption": "Wie heißen die 'kleinen' Programme, die direkt auf der GPU laufen?",
+							"answers": [
+								{
+									"answer": "img/spieletechnik/answer1.jpg",
+									"caption": "Shader"
+								}
+							]
+						},
+						{
+							"question": "img/logo.png",
+							"caption": "Wie nennt man die Methode, mit der während der Ausführung eines Spiels Programminhalte dynamisch generiert werden?",
+							"answers": [
+								{
+									"answer": "img/spieletechnik/answer2.png",
+									"caption": "Prozedurale Synthese / Prozedurale Generierung"
+								}
+							]
+						},
+						{
+							"question": "img/spieletechnik/question3.png",
+							"caption": "Wofür ist dieses Stück Code berühmt? Aus welchem Spiel stammt es?",
+							"answers": [
+								{
+									"answer": "img/spieletechnik/answer3.jpg",
+									"caption": "Es berechnet sehr effizient eine Näherung für 1/&radic;x"
+								}
+							]
+						},
+						{
+							"question": "img/logo.png",
+							"caption": "Wie hieß der erste für Privatkunden verfügbare 3D-Beschleuniger?",
+							"answers": [
+								{
+									"answer": "img/spieletechnik/answer4.jpg",
+									"caption": "3dfx Voodoo"
+								}
+							]
+						},
+						{
+							"question": "img/spieletechnik/question5.jpg",
+							"caption": "Was ist der Zweck von und der Unterschied zwischen 'V-Sync' und 'G-Sync'? ",
+							"answers": [
+								{
+									"answer": "img/spieletechnik/question5.jpg",
+									"caption": "Screen Tearing vermeiden<br><b>V-Sync</b>: neue Bilder werden immer nur zum Start des neuen Bildaufbaus an den Monitor gesendet<br><b>G-Sync</b>: der Monitor gibt eine neues Bild aus, wenn eins verfügbar ist"
+								}
+							]
+						},
+						
+						
+					
+					]
+
 				},
-				{  
-				   "question":"1.4.jpg",
-				   "answers":[  
-					  "Loesungen/1.4.jpg"
-				   ]
+				{ 
+					"category": "MCU",
+					"questions": [
+						{
+							"question": "img/mcu/question1.jpg",
+							"caption": "Wie nennt sich Peter Jason Quill selbst?",
+							"answers": [
+								"img/mcu/answer1.mp4"
+							]
+						},
+						{
+							"question": "img/mcu/question2.jpg",
+							"caption": "Wie viele Infinity-Steine gibt es?",
+							"answers": [
+								"img/mcu/answer2.mp4"
+							]
+						},
+						{
+							"question": "img/mcu/question3.png",
+							"caption": "Auf welchem Planeten herrscht der 'Grandmaster'? Xandar, Sakaar, Vormir oder Asgard?",
+							"answers": [
+								"img/mcu/answer3.mp4"
+							]
+								
+						},
+						{
+							"question": "img/mcu/question4.jpg",
+							"caption": "Wer verwendet die Infinity-Steine, um das halbe Universum zurückzuholen?",
+							"answers": [
+								"img/mcu/answer4.mp4"
+							]
+						},
+						{
+							"question": "img/mcu/question5.png",
+							"caption": "Was braucht Doctor Strange von Thor, um ihn zu seinem Vater zu führen?",
+							"answers": [
+								"img/mcu/answer5.mp4"
+							]
+						}
+					],
 				},
-				{  
-				   "question":"1.5.jpg",
-				   "answers":[  
-					  "Loesungen/1.5.1.jpg",
-			  "Loesungen/1.5.2.jpg"
-				   ]
-				},
-				{  
-				   "question":"1.6.jpg",
-				   "answers":[  
-					  "Loesungen/1.6.jpg",
-				   ]
+				{
+					"category": "IT-Kram",
+					"questions": [
+						{
+							"question": "img/it-kram/question1.png",
+							"caption": "Welche Windows-Desktop-Versionen (einzeln erwerbbar) wurden veröffentlicht? <small>[Hint: 17+2]</small>",
+							"answers": [
+								"img/it-kram/answer1.png"
+							]
+						},
+						{
+							"question": "img/it-kram/question2.png",
+							"caption": "Warum blieben Windows 95 und Windows 98 nach ungefähr 50 Tagen Uptime einfach stehen?",
+							"answers": [
+								{ "answer": "img/it-kram/answer2.png", "caption": "Die System Ticks wurden in einer 32-Bit-Variablen gespeichert. Beim Überlauf kam es zum Absturz." }
+							]
+						},
+						{
+							"question": "img/it-kram/question3.png",
+							"caption": "Wie schnell waren die ersten T-DSL-Leitungen, die ab Juli 1999 von der Telekom für Privatkunden verfügbar waren?",
+							"answers": [
+								{ "answer": "img/it-kram/question3.png", "caption": "768 kbit/s [ = 96 KiB/s!!!111 ]"}
+							]
+						},
+						{
+							"question": "img/it-kram/question4.jpg",
+							"caption": "Wie hieß die erste bekannte Internet-Filesharing-Börse?",
+							"answers": [
+								{ "answer": "img/it-kram/answer4.jpg" }
+							]
+						},
+						{
+							"question": "img/it-kram/question5.jpg",
+							"caption": "Was ist der wesentlichste Unterschied zwischen einem 'Hub' und einem 'Switch'?",
+							"answers": [
+								{ "answer": "img/it-kram/answer5.gif" }
+							]
+						},
+					]
 				}
-			 
-			 ]
-		},
-
-		{"category": "Shooter",
-		"questions":[
-			{
-			"question":"2.1.jpg",
-			"answers":[
-			  "Loesungen/2.1.jpg"
-			]
-			},
-			{
-			"question":"2.2.jpg",
-			"answers":[
-			  "Loesungen/2.2.jpg"
-			]
-			},
-			{
-			"question":"2.3.jpg",
-			"answers":[
-			  "Loesungen/2.3.jpg"
-			]
-			},			    {
-			"question":"2.4.jpg",
-			"answers":[
-			  "Loesungen/2.4.jpg"
-			]
-			},			    {
-			"question":"2.5.jpg",
-			"answers":[
-			  "Loesungen/2.5.jpg"
-			]
-			},			    {
-			"question":"2.6.jpg",
-			"answers":[
-			  "Loesungen/2.6.mp4"
-			]
-			},
-		]},
-		{"category": "Mystery",
-		"questions":[
-			{
-			"question":"3.1.jpg",
-			"answers":[
-			  "Loesungen/3.1.jpg"
-			]
-			},
-			{
-			"question":"3.2.jpg",
-			"answers":[
-			  "Loesungen/3.2.mp4"
-			]
-			},
-			{
-			"question":"3.3.jpg",
-			"answers":[
-			  "Loesungen/3.3.jpg"
-			]
-			},
-			{
-			"question":"3.4.jpg",
-			"answers":[
-			  "Loesungen/3.4.jpg"
-			]
-			},
-			{
-			"question":"3.5.jpg",
-			"answers":[
-			  "Loesungen/3.5.jpg"
-			]
-			},
-			{
-			"question":"3.6.jpg",
-			"answers":[
-			  "Loesungen/3.6.jpg"
-			]
-			},
-
-		]},
-		{
-			"category": "Filme und Serien",
-			"questions": [
-				{
-					"question":"4.1.jpg",
-					"answers":[
-						"Loesungen/4.1.jpg"
-					]
-				},
-				{
-					"question":"4.2.jpg",
-					"answers":[
-						"Loesungen/4.2.mp4"
-					]
-				},
-				{
-					"question":"4.3.jpg",
-					"answers":[
-						"Loesungen/4.3.mp4"
-					]
-				},
-				{
-					"question":"4.4.jpg",
-					"answers":[
-						"Loesungen/4.4.mp4"
-					]
-				},
-				{
-					"question":"4.5.jpg",
-					"answers":[
-						"Loesungen/4.5.mp4"
-					]
-				},
-{
-					"question":"4.6.mp4",
-					"answers":[
-						"Loesungen/4.6.jpg"
-					]
-				},
-			]
-		}
-			
-
 		],
-	   "grouprounds": [
-		   {
+		"grouprounds": [
+			{
 				"title": "Gruppenrunde 1",
 				"questions":
-			   [
-				   {
-					   "question":"4.5.jpg",
-					   "questionUnblur": true,
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.2.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.3.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.4.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-						"question":"G1.5.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.6.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.7.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.8.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.9.mp4",
-					   "answers":[
-					   ]
-				   },
-				   {
-					   "question":"G1.10.mp4",
-					   "answers":[
-					   ]
-				   },
-			   ]
-		   },
-		   {
+					[
+						{
+							"question": "img/g1/q1.mp4",
+							"answers": [
+								"img/g1/a1.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q2.mp4",
+							"answers": [
+								"img/g1/a2.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q3.mp4",
+							"answers": [
+								"img/g1/a3.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q4.mp4",
+							"answers": [
+								"img/g1/a4.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q5.mp4",
+							"answers": [
+								"img/g1/a5.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q6.mp4",
+							"answers": [
+								"img/g1/a6.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q7.mp4",
+							"answers": [
+								"img/g1/a7.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q8.mp4",
+							"answers": [
+								"img/g1/a8.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q9.mp4",
+							"answers": [
+								"img/g1/a9.mp4"
+							]
+						},
+						{
+							"question": "img/g1/q10.mp4",
+							"answers": [
+								"img/g1/a10.mp4"
+							]
+						},
+					]
+			},
+			{
 				"title": "Gruppenrunde 2",
 				"questions":
-			   [
-				   {
-					   "question":"G2.1.jpg",
-					   "answers":["Loesungen/G2.1.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.2.jpg",
-					   "answers":["Loesungen/G2.2.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.3.jpg",
-					   "answers":["Loesungen/G2.3.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.4.jpg",
-					   "answers":["Loesungen/G2.4.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.5.jpg",
-					   "answers":["Loesungen/G2.5.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.6.jpg",
-					   "answers":["Loesungen/G2.6.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.7.jpg",
-					   "answers":["Loesungen/G2.7.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.8.jpg",
-					   "answers":["Loesungen/G2.8.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.9.jpg",
-					   "answers":["Loesungen/G2.9.jpg"
-					   ]
-				   },
-				   {
-					   "question":"G2.10.jpg",
-					   "answers":["Loesungen/G2.10.jpg"
-					   ]
-				   },
-			   ]               
-		   },
-		   {
+					[
+						{
+							"question": "G2.1.jpg",
+							"answers": ["Loesungen/G2.1.jpg"
+							]
+						},
+						{
+							"question": "G2.2.jpg",
+							"answers": ["Loesungen/G2.2.jpg"
+							]
+						},
+						{
+							"question": "G2.3.jpg",
+							"answers": ["Loesungen/G2.3.jpg"
+							]
+						},
+						{
+							"question": "G2.4.jpg",
+							"answers": ["Loesungen/G2.4.jpg"
+							]
+						},
+						{
+							"question": "G2.5.jpg",
+							"answers": ["Loesungen/G2.5.jpg"
+							]
+						},
+						{
+							"question": "G2.6.jpg",
+							"answers": ["Loesungen/G2.6.jpg"
+							]
+						},
+						{
+							"question": "G2.7.jpg",
+							"answers": ["Loesungen/G2.7.jpg"
+							]
+						},
+						{
+							"question": "G2.8.jpg",
+							"answers": ["Loesungen/G2.8.jpg"
+							]
+						},
+						{
+							"question": "G2.9.jpg",
+							"answers": ["Loesungen/G2.9.jpg"
+							]
+						},
+						{
+							"question": "G2.10.jpg",
+							"answers": ["Loesungen/G2.10.jpg"
+							]
+						},
+					]
+			},
+			{
 				"title": "Stichfragen",
 				"questions":
-			   [
-				   {
-					   "question":"S1.jpg",
-					   "answers":[
-						   "Loesungen/S1.jpg"
-					   ]
-				   },
-				   {
-					   "question":"S2.jpg",
-					   "answers":[
-						   "Loesungen/S2.jpg"
-					   ]
-				   },
-				   {
-					   "question":"S3.jpg",
-					   "answers":[
-						   "Loesungen/S3.jpg"
-					   ]
-				   },
-			   ]               
-		   },
-		   
-	   ]
+					[
+						{
+							"question": "S1.jpg",
+							"answers": [
+								"Loesungen/S1.jpg"
+							]
+						},
+						{
+							"question": "S2.jpg",
+							"answers": [
+								"Loesungen/S2.jpg"
+							]
+						},
+						{
+							"question": "S3.jpg",
+							"answers": [
+								"Loesungen/S3.jpg"
+							]
+						},
+					]
+			},
+		]
 	}
--- quiz.js
+++ quiz.js
@@ -1,9 +1,9 @@
 
 	function getStateValue(param)
 	{
-		string = window.location.hash;
+		var string = window.location.hash;
 		string = string.substring(1);
-		values = string.split("~");
+		var values = string.split("~");
 		return $.inArray(param, values) != -1;
 	}
 
@@ -77,10 +77,10 @@
 
 	function showOverview()
 	{
-		html = "<div class='overviewContainer'><h1>" + d.title + "</h1><table><thead><tr>";
+		var html = "<div class='overviewContainer'><h1>" + d.title + "</h1><table><thead><tr>";
 
 		var maxCount = 0;
-		for (idx in d.categories)
+		for (var idx in d.categories)
 		{
 			html += '<td>' + d.categories[idx].category + '</td>';
 			maxCount = Math.max(maxCount, d.categories[idx].questions.length);
@@ -92,20 +92,11 @@
 			html += '<tr>'
 			for (idx in d.categories)
 			{
-				qId = idx + '/' + i
+				var qId = idx + '/' + i
+				var asked;
 				if (getStateValue(qId)) { asked = "asked " } else { asked = "" }
-				if (d.categories[idx].questions[i].caption == null) {
-					caption = i+1;
-				} else {
-					caption = d.categories[idx].questions[i].caption;
-				}
 				html += '<td';
-				if (typeof d.categories[idx].questions[i] != 'undefined') 
-				{ 
-					html += ' class="' + asked + 'qLink" data-catId="' + idx + '" data-qId="' + i + '">' + caption; 
-				}  else { 
-					html += ' class="asked">'; 
-				}
+				if (typeof d.categories[idx].questions[i] != 'undefined') { html += ' class="' + asked + 'qLink" data-catId="' + idx + '" data-qId="' + i + '">' + (i+1); }  else { html += ' class="asked">'; }
 				html += '</td>';
 			}		
 			html += '</tr>';
@@ -124,16 +115,21 @@
         html += "</tr></thead></table></div>";
 
 		$(".wrapper").html(html);
+		$("#caption").html("");
 		$("td[data-qid]").click(function () { showQuestion($(this).attr("data-catId"), $(this).attr("data-qId")); });
         $("td[data-grId]").click(function () { showGroupround($(this).attr("data-grId")); });
 	}
 
 	function showQuestion(cat, q)
 	{
-		question = d.categories[cat].questions[q].question;
-		questionUnblur = d.categories[cat].questions[q].questionUnblur;
-			
-		$(".wrapper").html(embedFile(question, questionUnblur));
+		var question = d.categories[cat].questions[q].question;
+		var caption = d.categories[cat].questions[q].caption;
+		var questionUnblur = d.categories[cat].questions[q].questionUnblur;
+		var loop = d.categories[cat].questions[q].loop;
+		var timer = d.categories[cat].questions[q].timer;
+		
+		$(".wrapper").html(embedFile(question, questionUnblur, loop, timer));
+		$("#caption").html(caption);
 		$("#link").click(function () { showAnswer(cat, q); }); 
 
         startTimer(90, $("#timer"));
@@ -141,9 +137,15 @@
 
 	function showAnswer(cat, q, idx = 0)
 	{
+		var answer = d.categories[cat].questions[q].answers[idx];
+		if (typeof answer !== 'object') {
+			answer = { "answer": answer };
+			answer.caption = "";
+		}
+		$(".wrapper").html(embedFile(answer.answer));
+		$("#caption").html("");
+		$("#caption").html(answer.caption);
 		
-		answer = d.categories[cat].questions[q].answers[idx];
-		$(".wrapper").html(embedFile(answer));
 		setStateValue(cat + "/" + q);
 		$("#link").click(function () 
 		{ 
@@ -159,15 +161,23 @@
 	}
 
     function showGroupround(id, qIdx = 0, aIdx = -1)
-    {
+	{
+		var file;
+		var unblur;
+		var loop;
+		var timer;
         if (aIdx == -1) {
             file = d.grouprounds[id].questions[qIdx].question;
 			unblur = d.grouprounds[id].questions[qIdx].questionUnblur;
+			loop = d.grouprounds[id].questions[qIdx].loop;
+			timer = d.grouprounds[id].questions[qIdx].timer;
         } else {
             file = d.grouprounds[id].questions[qIdx].answers[aIdx];            
 			unblur = false;
+			loop = false;
+			timer = false;
         }
-        $(".wrapper").html(embedFile(file, unblur));
+        $(".wrapper").html(embedFile(file, unblur, loop, timer));
         setStateValue("gr/"+id);
 
 		$("#link").click(function () 
@@ -192,19 +202,34 @@
         
     }
       
-    function embedFile(file, unblur)
+    function embedFile(file, unblur, timer, loop = false)
     {
-        filetype = getFiletype(file)
+		var filetype = getFiletype(file);
+		var html;
+		if (filetype === "none") {
+			html = "<a id='link'>";
+			html += "</a>"
+		}
 		if (filetype.startsWith("video/"))
 		{
 			html = "<a id='link'>";
-			html += "<div class='videoContainer'><video autoplay controls><source src='" + file + "' type='" + filetype + "'></video></div>"
+			// timer defaults to false for videos
+			if (timer === true) {
+				html += "<div id='timer'></div>";
+			}
+			
+			var loopstr;
+			if (loop) { loopstr = " loop"; } else { loopstr = ""; }
+			html += "<div class='videoContainer'><video autoplay controls" + loopstr + "><source src='" + file + "' type='" + filetype + "'></video></div>";
 		}
 		if (filetype.startsWith("image/") && !unblur)
 		{
 			html = "<a id='link'>";
-            // timer only makes sense for static content
-			html += "<div id='timer'></div><div class='imageContainer'><img src='" + file + "'></div>"
+			// timer defaults to true for images
+			if (timer !== false) {
+				html += "<div id='timer'></div>";
+			}
+			html += "<div class='imageContainer' > <img src='" + file + "'></div>";
 			html += "</a>";
 		}
 		if (filetype.startsWith("image/") && unblur)
@@ -215,6 +240,10 @@
 		}
         if (filetype.startsWith("audio/"))
         {
+			// timer defaults to false for audio
+			if (timer === true) {
+				html += "<div id='timer'></div>";
+			}
 			html = "<a id='link'>";
             html += "<div class='audioContainer'><audio src='" + file + "' autoplay controls></audio></div>"        
 			html += "</a>";
@@ -246,5 +275,9 @@
 		if (filename.toLowerCase().endsWith('.mp4')) return "video/mp4";
 		if (filename.toLowerCase().endsWith('.jpg')) return "image/jpeg";
 		if (filename.toLowerCase().endsWith('.png')) return "image/png";
+		if (filename.toLowerCase().endsWith('.gif')) return "image/gif";
+		if (filename.toLowerCase().endsWith('.webp')) return "image/webp";
 		if (filename.toLowerCase().endsWith('.mp3')) return "audio/mpeg";
+		if (filename.toLowerCase().endsWith('.svg')) return "image/svg+xml";
+		return "none";
 	}
